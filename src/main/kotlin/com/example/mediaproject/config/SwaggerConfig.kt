6910f723bca37f240e7c798bdbffa3a66a7b2c0a
typebi <typebi.dev@gmail.com>
2021-12-12 02:58:48 +0900

    Add swagger config

--- src/main/kotlin/com/example/mediaproject/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/mediaproject/config/SwaggerConfig.kt
@@ -1,10 +1,15 @@
 package com.example.mediaproject.config
 
+import com.google.common.collect.Lists
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.SecurityScheme
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -16,15 +21,15 @@ class SwaggerConfig {
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-//            .securityContexts(Lists.newArrayList(securityContext()))
+            .apis(RequestHandlerSelectors.basePackage("com.example.mediaproject.api.rest"))
             .build()
-//            .securitySchemes(Lists.newArrayList(apiKey()) as List<SecurityScheme>)
+            .securityContexts(Lists.newArrayList(securityContext()))
+            .securitySchemes(Lists.newArrayList(apiKey()) as List<SecurityScheme>)
     }
 
-    /*
-
+    fun apiKey(): ApiKey {
+        return ApiKey("JWT", "Authorization", "header")
+    }
 
     fun securityContext(): SecurityContext {
         return SecurityContext.builder()
@@ -38,10 +43,4 @@ class SwaggerConfig {
         authorizationScopes[0] = authorizationScope
         return Lists.newArrayList(SecurityReference("JWT", authorizationScopes))
     }
-
-    fun apiKey(): ApiKey {
-        return ApiKey("JWT", "Authorization", "header")
-    }
-
-     */
 }
